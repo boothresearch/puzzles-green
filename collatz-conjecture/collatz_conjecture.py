552edf6951288ee58aaae26f55aac0d260fb0568
Lila <lila.englander@gmail.com>
2021-09-14 11:52:01 -0500

    add iteration count

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,13 +1,16 @@
 def steps(number):
+    count = 0
     if number > 0:
         while number !=1:
             #if even, divide until odd
             while number % 2 == 0:
                     number = number / 2 
+                    count +=1
             #if odd, 3n+1
             while number % 2 != 0 and number !=1:
                 number = 3*number+1
-        return(number)
+                count +=1
+        return(count)
     else:
         return(ValueError)
 
