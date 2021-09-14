544496746dc0fc6c7b9a8e21df661436fd3c386e
Lila <lila.englander@gmail.com>
2021-09-14 10:30:37 -0500

    fixed not 100

--- leap/leap.py
+++ leap/leap.py
@@ -2,4 +2,6 @@ def leap_year(year):
     hun = year/100
     fhun = year/400
     four = year/4
-    print(four.is_integer() and not (hun.is_integer() and fhun.is_integer()))
+    print(four.is_integer() and (not hun.is_integer() and fhun.is_integer()))
+
+leap_year(2100)
