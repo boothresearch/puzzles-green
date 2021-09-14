f0e44b0fe0061f772d4c7c80783237be877c86b3
Lila <lila.englander@gmail.com>
2021-09-14 10:59:39 -0500

    WIP

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
 def steps(number):
-    pass
+    if number > 0:
+        #check if odd
+        even = number/2
+        if even.is_integer():
+            #divide by 2 until number is odd
+            #then 3n+1 until equal to 1
+            3*number+1
+        else:
+             #then 3n+1 until equal to 1
+    else:
+        print("Negative or 0")
