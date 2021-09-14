86dc1cff2e6d714111161d13ceb2b9fa60529892
lenglander <86369924+lenglander@users.noreply.github.com>
2021-09-14 11:55:23 -0500

    Le collatz (#2)
    * collatz-conjecture solution, passes the test

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,16 @@
 def steps(number):
-    pass
+    count = 0
+    if number > 0:
+        while number !=1:
+            #if even, divide until odd
+            while number % 2 == 0:
+                    number = number / 2 
+                    count +=1
+            #if odd, 3n+1
+            while number % 2 != 0 and number !=1:
+                number = 3*number+1
+                count +=1
+        return(count)
+    else:
+        raise ValueError("This is a 0 or negative number")
+
