983451bf8482c2edd1488f1057a231bb66e50feb
Wendy Wen <wendy.wen@chicagobooth.edu>
2021-09-14 11:30:33 -0500

    v1

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -2,5 +2,13 @@ def find_anagrams(word, candidates):
     letters = list(word)
     for candidate in candidates:
         can_letters = list(candidate)
-        
+        correct = 1
+        for can_letter in can_letters:
+            if can_letter not in letters:
+                correct = 0
+        for letter in letters:
+            if letter not in can_letter:
+                correct == 0
+        if correct == 1:
+            return candidate
 
