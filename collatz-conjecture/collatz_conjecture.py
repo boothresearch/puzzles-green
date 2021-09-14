6978922e115afa6968cb4a933278966158b1c60f
Lila <lila.englander@gmail.com>
2021-09-14 11:46:51 -0500

    now it gets to 1!

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -5,11 +5,9 @@ def steps(number):
             while number % 2 == 0:
                     number = number / 2 
             #if odd, 3n+1
-            while number % 2 != 0:
+            while number % 2 != 0 and number !=1:
                 number = 3*number+1
         return(number)
     else:
         return(ValueError)
-
-steps(12)
 
