873366bc4e5ea621a5144354ed3fee015c49ec74
yingxuan <yingxuanyang0@gmail.com>
2021-09-14 10:52:15 -0500

    Complete the second question

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,12 @@
 def score(x, y):
-    pass
+    distance=(x**2+y**2)**0.5
+    if distance>10:
+       reward=0 
+    elif distance<=10 and distance>5:
+       reward=1
+    elif distance<=5 and distance>1:
+        reward=5
+    elif distance<=1:
+        reward=10
+
+    return reward
