00a58b9cf2aefa32f48e7852e44da018aab3d7b7
Lila <lila.englander@gmail.com>
2021-09-14 11:31:06 -0500

    still isn't working!
    Yargh

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,15 +1,15 @@
 def steps(number):
     if number > 0:
         while number !=1:
-            #if odd, 3n+1
-            while number % 2 != 0:
-                number = 3*number+1
             #if even, divide until odd
             while number % 2 == 0:
                     number = number / 2 
             #if odd, 3n+1
             while number % 2 != 0:
                 number = 3*number+1
+        return(number)
     else:
         print("Negative or 0")
+
+steps(12)
 
