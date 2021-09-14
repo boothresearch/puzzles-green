abb47bef48ee1aeebe26a276199a7c9c3fef64f5
Lila <lila.englander@gmail.com>
2021-09-14 10:33:04 -0500

    add return

--- leap/leap.py
+++ leap/leap.py
@@ -2,6 +2,4 @@ def leap_year(year):
     hun = year/100
     fhun = year/400
     four = year/4
-    print(four.is_integer() and (not hun.is_integer() and fhun.is_integer()))
-
-leap_year(2100)
+    return(four.is_integer() and (not hun.is_integer() and fhun.is_integer()))
